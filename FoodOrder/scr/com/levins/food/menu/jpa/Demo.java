dabce26dbdf70c6fe690ccf0f12ab5c003d16892
Krachunov <Krachunov@Krachunov-PC>
2016-03-27 12:32:21 +0300

    Remove unnecessary code line in main class

--- FoodOrder/scr/com/levins/food/menu/jpa/Demo.java
+++ FoodOrder/scr/com/levins/food/menu/jpa/Demo.java
@@ -16,37 +16,9 @@ import com.levins.food.menu.ui.OrderMenu;
 
 public class Demo {
 
-	public static List<Employee> getAllEmployees()
-			throws ClassNotFoundException {
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("FoodMenu");
-		EntityManager entityManager = factory.createEntityManager();
-
-		String stringQuery = String.format("from * s where (s.Employee");
-		System.out.println(stringQuery);
-		Query query = entityManager.createQuery(stringQuery);
-		@SuppressWarnings("unchecked")
-		List<Employee> list = query.getResultList();
-
-		entityManager.close();
-		return list;
-	}
-
-	// "dd_MM_yyyy" "dd_MM_yyyy':'HH:mm:"
-	public static String createdDate(Date date) {
-		String format = "dd/MMMM/yyyy' - 'HH:mm:";
-		DateFormat df = new SimpleDateFormat(format);
-		Date today = Calendar.getInstance().getTime();
-		String reportDate = df.format(today);
-		return reportDate;
-	}
-
 	public static void main(String[] args) throws IOException {
 
 		OrderMenu menu = new OrderMenu();
 		menu.setVisible(true);
-
-//		Date d = new Date();
-//		System.out.println(createdDate(d));
 	}
 }
